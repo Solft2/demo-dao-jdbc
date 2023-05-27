0c20326f07df32848d2aa7387bdbc7935cf7824c
Solft2 <arthureric33@gmail.com>
2023-05-27 10:26:42 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
    public static void main(String[] args) {
 	   Department obj = new Department(1,"books");
-	   System.out.println(obj);
+	   
+	   Seller sell = new Seller(21,"João","joão@gmail.com",new Date(),3000.0,obj);
+	   System.out.println(sell);
    } 
 }
