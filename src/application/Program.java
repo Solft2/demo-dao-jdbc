0c91dc29a11d3203e370a4346dcb1dca8f20937e
Solft2 <arthureric33@gmail.com>
2023-05-29 10:03:08 -0300

    Instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,14 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
    public static void main(String[] args) {
 	   
 	   SellerDao selldao = DaoFactory.createSellerDao();
+	   System.out.println("----TEST----");
 	   Seller sell = selldao.findById(3);
 	   System.out.println(sell);
    } 
