e28b029a97b52a87839a761e0c6c7aa0be159c18
Solft2 <arthureric33@gmail.com>
2023-05-29 11:04:29 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,5 +26,9 @@ public class Program {
 	   for(Seller obj : list) {
 		   System.out.println(obj);
 	   }
+	   System.out.println("----TEST 3 ----");
+	   Seller newSeller = new Seller(null,"Greg","Greg@gmail.com",new Date(),4000.0,department);
+	   selldao.insert(newSeller);
+	   System.out.println("Adicionado: " + newSeller.getId());
    } 
 }
